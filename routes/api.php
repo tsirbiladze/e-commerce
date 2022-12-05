550ad478c99107840da099672ea0e665c93d8f8f
tsirbiladze <tsirbiladz@gmail.com>
2022-12-05 17:44:51 +0400

    Category index endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,8 @@
 <?php
 
+use App\Http\Controllers\Categories\CategoryController;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    $categories = Category::parents()->get();
-
-    dd($categories);
-});
+Route::resource('categories', CategoryController::class);
