a3c857e1367ca0521e9d18e05e3f43fd61f36016
tsirbiladze <tsirbiladz@gmail.com>
2022-12-14 15:12:00 +0400

    Product stock blocks

--- tests/Unit/Product/ProductVariationTest.php
+++ tests/Unit/Product/ProductVariationTest.php
@@ -6,6 +6,7 @@ use App\Cart\Money;
 use App\Models\Product;
 use App\Models\ProductVariation;
 use App\Models\ProductVariationType;
+use App\Models\Stock;
 use Tests\TestCase;
 
 class ProductVariationTest extends TestCase
@@ -52,5 +53,17 @@ class ProductVariationTest extends TestCase
         ]);
 
         $this->assertTrue($variation->priceVaries());
+    }
+
+    public function test_it_has_many_stocks()
+    {
+        $variation = ProductVariation::factory()->create();
+
+        $variation->stocks()->save(
+            Stock::factory()->make()
+        );
+
+        $this->assertInstanceOf(Stock::class, $variation->stocks->first());
+
     }
 }
