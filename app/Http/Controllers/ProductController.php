8be47db4a496fb3d1680069fb9a90adfa2812c93
tsirbiladze <tsirbiladz@gmail.com>
2022-12-26 18:58:00 +0400

    JSON response profiling

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,13 +12,14 @@ class ProductController extends Controller
 {
     public function index()
     {
-        $products = Product::withScopes($this->scopes())->paginate(10);
+        $products = Product::with(['variations.stock'])->withScopes($this->scopes())->paginate(10);
 
         return ProductIndexResource::collection($products);
     }
 
     public function show(Product $product)
     {
+        $product->load(['variations.stock', 'variations.type', 'variations.product']);
         return new ProductResource($product);
     }
 
